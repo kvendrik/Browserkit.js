036da9516a16c195f76c304ef9fabd635af7c798
Koen Vendrik <k.vendrik@gmail.com>
2015-01-08 20:55:11 +0100

    Adds scrollTop method, tests and documentation

---
+++ src/methods/scrolltop.js
@@ -0,0 +1,42 @@
+(function(){
+	
+	var getWindowScrollTop,
+		docEl;
+
+	_defineMethod('scrollTop', function(newY){
+		var el = this[0];
+
+		//on first use init docEl and getWindowScrollTop vars
+		if(!docEl){
+			docEl = (document.documentElement || document.body.parentNode || document.body);
+		}
+
+		if(!getWindowScrollTop){
+			if(window.pageYOffset !== undefined){
+				getWindowScrollTop = function(){
+					return window.pageYOffset;
+				};
+			} else {
+				getWindowScrollTop = function(){
+					return docEl.scrollTop;
+				};
+			}
+		}
+
+		if(newY){
+			if(el === window){
+				el.scrollTo(0, newY);
+			} else {
+				el.scrollTop = newY;
+			}
+			return this;
+		} else {
+			if(el === window){
+				return getWindowScrollTop();
+			} else {
+				return el.scrollTop;
+			}
+		}
+	});
+
+}());
