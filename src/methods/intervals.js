0bfac2aaf7775395080e39cd6100f26a4515df40
Koen Vendrik <k.vendrik@gmail.com>
2015-01-08 22:33:32 +0100

    Enhances setInterval, adds clearInterval, adds tests and documentation for new methods

---
+++ src/methods/intervals.js
@@ -0,0 +1,61 @@
+_defineMethod('setInterval', function(handler, delay){
+
+    //init intervalCache to store interval ID's
+    if(typeof this.intervalCache !== 'object'){
+        this.intervalCache = {
+            currIdx: 0,
+            cache: {}
+        };
+    }
+
+    var self = this,
+        intervalCache = this.intervalCache,
+        cache = intervalCache.cache,
+        newId = intervalCache.currIdx++;
+
+    //save a space for the new id
+    cache[newId] = 0;
+
+    var setSafeInterval = function(handler, delay){
+        if(typeof cache[newId] !== 'number') return;
+
+        //user calls the method
+        //a setTimout with ID 1 is returned
+        return setTimeout(function(){
+
+            //if the previous interval is not cleared
+            //the id count will continue in order
+
+            //problem here is that the user only has reference
+            //to the setTimout with ID 1
+            //setTimeout with ID 1 has already been triggered
+            //and the current ID can be already 10
+            //this makes it impossble to clear the correct Interval
+            //solution is to keep track of the current id
+
+            handler();
+            cache[newId] = setSafeInterval(handler, delay);
+
+        }, delay);
+
+    };
+
+    cache[newId] = setSafeInterval(handler, delay);
+
+    return newId;
+
+}, false);
+
+_defineMethod('clearInterval', function(intervalId){
+
+    if(typeof this.intervalCache === 'object'){
+        var cache = this.intervalCache.cache,
+            originalId = cache[intervalId];
+
+        if(typeof originalId === 'number'){
+            delete cache[intervalId];
+            return window.clearTimeout(originalId);
+        }
+    }
+
+}, false);
